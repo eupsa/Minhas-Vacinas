49fd8db1fe0148611c9ab75e95ca7993ac2a6380
Pedro Araujo <106033591+psilvagg@users.noreply.github.com>
2024-10-06 00:37:47 -0300

    .

--- metodos/cadastro.php
+++ metodos/cadastro.php
@@ -8,47 +8,46 @@ require 'config.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-$nome = $_POST['nome'];
-$email = $_POST['email'];
+$nome = strtolower($_POST['nome']);
+$email = strtolower($_POST['email']);
 $estado = $_POST['estado'];
 $senha = $_POST['senha'];
 $confsenha = $_POST['confSenha'];
 
-enviarEmail($nome, $email);
+if ($senha == $confsenha) {
+    try {
+        $senhaHash = hash('sha256', $senha);
+        $sql = $pdo->prepare("INSERT INTO usuarios (nome, email, estado, senha) VALUES (:nome, :email, :estado, :senha)");
+        $sql->bindValue(':nome', $nome);
+        $sql->bindValue(':email', $email);
+        $sql->bindValue(':estado', $estado);
+        $sql->bindValue(':senha', $senhaHash);
+        $sql->execute();
 
-try {
-    $senhaHash = hash('sha256', $senha);
-
-    $sql = $pdo->prepare("INSERT INTO usuarios (nome, email, estado, senha) VALUES (:nome, :email, :estado, :senha)");
-    $sql->bindValue(':nome', $nome);
-    $sql->bindValue(':email', $email);
-    $sql->bindValue(':estado', $estado);
-    $sql->bindValue(':senha', $senhaHash);
-    $sql->execute();
-
-    if ($sql->execute() == true) {
         enviarEmail($nome, $email);
-        header("Location: ../index.html");
+        header("Location: ../entrar/index.html");
         exit;
+    } catch (PDOException $e) {
+        // Capturando erros específicos
+        switch ($e->getCode()) {
+            case 23000: // Duplicidade de entrada
+                echo "Esse e-mail já está cadastrado. Por favor, use um e-mail diferente.";
+                break;
+            case 42000: // Erro de sintaxe SQL
+                echo "Erro no banco de dados. Por favor, tente novamente mais tarde.";
+                break; // Erro de conexão
+                //case 08004:
+                //echo "Não foi possível conectar ao banco de dados. Por favor, verifique a conexão.";
+                //break;
+            case 22007: // Formato de dados inválido
+                echo "Formato de dados inválido. Por favor, verifique os campos e tente novamente.";
+                break;
+            default: // Mensagem genérica para outros erros
+                echo "Erro ao cadastrar: " . $e->getMessage();
+        }
     }
-} catch (PDOException $e) {
-    // Capturando erros específicos
-    switch ($e->getCode()) {
-        case 23000: // Duplicidade de entrada
-            echo "Esse e-mail já está cadastrado. Por favor, use um e-mail diferente.";
-            break;
-        case 42000: // Erro de sintaxe SQL
-            echo "Erro no banco de dados. Por favor, tente novamente mais tarde.";
-            break; // Erro de conexão
-            //case 08004:
-            //echo "Não foi possível conectar ao banco de dados. Por favor, verifique a conexão.";
-            //break;
-        case 22007: // Formato de dados inválido
-            echo "Formato de dados inválido. Por favor, verifique os campos e tente novamente.";
-            break;
-        default: // Mensagem genérica para outros erros
-            echo "Erro ao cadastrar: " . $e->getMessage();
-    }
+} else {
+    echo "As senhas estão diferentes.";
 }
 
 function enviarEmail($nome, $email)
--- metodos/entrar.php
+++ metodos/entrar.php
@@ -4,27 +4,39 @@ require 'config.php';
 
 $email = $_REQUEST['email'];
 $senha = $_REQUEST['senha'];
-
 $senhaHash = hash('sha256', $senha);
 
-try {
-    $sql = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email AND senha = :senha");
-    $sql->bindValue(':email', $email);
-    $sql->bindValue(':senha', $senhaHash);
-    $sql->execute();
+function entrar($pdo, $email, $senha, $senhaHash)
+{
+    try {
+        $sql = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email AND senha = :senha");
+        $sql->bindValue(':email', $email);
+        $sql->bindValue(':senha', $senhaHash);
+        $sql->execute();
 
-    if ($sql->rowCount() > 0) {
-        $usuario = $sql->fetch(PDO::FETCH_BOTH);    
-        
-        $_SESSION['user_id'] = $usuario['id'];
-        $_SESSION['user_nome'] = $usuario['nome'];
-        $_SESSION['user_email'] = $usuario['email'];
-        $_SESSION['user_cidade'] = $usuario['cidade'];
-        header("Location: ../painel/index.html");
-        exit;
-    } else {
-        echo "E-mail ou senha incorretos. Tente novamente.";
+        if ($sql->rowCount() > 0) {
+            $usuario = $sql->fetch(PDO::FETCH_BOTH);
+
+            $_SESSION['user_id'] = $usuario['id'];
+            $_SESSION['user_nome'] = $usuario['nome'];
+            $_SESSION['user_email'] = $usuario['email'];
+            header("Location: ../painel/index.html");
+            exit;
+        } else {
+            echo "E-mail ou senha incorretos. Tente novamente.";
+        }
+    } catch (PDOException $e) {
+        echo "Erro ao acessar o banco de dados: " . $e->getMessage();
     }
-} catch (PDOException $e) {
-    echo "Erro ao acessar o banco de dados: " . $e->getMessage();
+}
+
+if (session_status() === PHP_SESSION_ACTIVE && !empty($_SESSION)) {
+    header("Location: ../painel/index.html");
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['login'])) {
+        entrar($pdo, $email, $senha, $senhaHash);
+    }
+} else {
+    header("Location: ../cadastro/index.html");
 }
