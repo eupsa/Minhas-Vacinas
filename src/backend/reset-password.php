0f2f0f5f5ab81d571ba3281ac4e1b01cf479ce74
Pedro <106033591+psilvagg@users.noreply.github.com>
2024-10-17 20:55:14 -0300

    update

--- src/backend/reset-password.php
+++ src/backend/reset-password.php
@@ -4,6 +4,7 @@ require '../../vendor\phpmailer\phpmailer\src\Exception.php';
 require '../../vendor\phpmailer\phpmailer\src\SMTP.php';
 require '../../vendor\autoload.php';
 require '../backend/scripts/conn.php';
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -22,13 +23,13 @@ if (empty($email)) {
         $sql->execute();
 
         if ($sql->rowCount() === 1) {
+            $usuario = $sql->fetch(PDO::FETCH_BOTH);
+            $nome = $usuario['nome'];
             $codeReset = rand(100000, 999999);
-
-
-
-            $retorna = ['status' => true, 'msg' => "Login bem-sucedido!"];
-            header('Content-Type: application/json');
-            echo json_encode($retorna);
+            enviarEmail($nome, $email, $codeReset);
+            // $retorna = ['status' => true, 'msg' => "Login bem-sucedido!"];
+            // header('Content-Type: application/json');
+            // echo json_encode($retorna);
         } else {
             $retorna = ['status' => false, 'msg' => "Usuário ou senha incorretos."];
             header('Content-Type: application/json');
@@ -40,5 +41,37 @@ if (empty($email)) {
         header('Content-Type: application/json');
         echo json_encode($retorna);
         exit();
+    }
+}
+
+function enviarEmail($nome, $email, $codeReset)
+{
+    $email_body = file_get_contents('../../assets/templates/register.php');
+    $email_body = str_replace('{{nome}}', $nome, $email_body);
+    $email_body = str_replace('{{codeReset}}', $codeReset, $email_body);
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'pedrooosxz@gmail.com';
+        $mail->Password = 'zolp wzgo pvcr ucpb';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        $mail->setFrom('pedrooosxz@gmail.com', 'php bonito');
+        $mail->addAddress($email);
+
+        $mail->isHTML(true);
+        $mail->Subject = 'hmmm php';
+        $mail->Body = $email_body;
+        $mail->AltBody = 'Este é o corpo do e-mail em texto plano, para clientes de e-mail sem suporte a HTML';
+
+        $mail->send();
+
+        return true;
+    } catch (Exception $e) {
+        return false;
     }
 }
