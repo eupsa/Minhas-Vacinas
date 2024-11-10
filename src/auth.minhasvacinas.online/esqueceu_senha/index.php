cdd3fcb5af99ed6e2394765adc905c10dfb5ca27
Pedro Araujo <106033591+psilvagg@users.noreply.github.com>
2024-11-09 21:46:25 -0300

    upadate

--- src/auth.minhasvacinas.online/esqueceu_senha/index.php
+++ src/auth.minhasvacinas.online/esqueceu_senha/index.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-    <title>Minhas Vacinas - Recuperação</title>
+    <title>Minhas Vacinas - Recuperação de Senha</title>
 </head>
 
 <body>
