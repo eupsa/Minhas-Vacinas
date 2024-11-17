dd8e956be58d967771bd64fb0cf329986599f7a2
Pedro Araujo <106033591+psilvagg@users.noreply.github.com>
2024-11-17 16:29:20 -0300

    upload

--- src/scripts/conn.php
+++ src/scripts/conn.php
@@ -1,3 +1,4 @@
 <?php
 $pdo = new PDO("mysql:dbname=minhas_vacinas;host=vacinas.cfmmccyw4yhv.sa-east-1.rds.amazonaws.com", "admin", "Chicote1");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
