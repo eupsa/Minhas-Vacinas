4d78f2628df3f6d26bb6e2ee674e27d72515e545
Pedro Silva <106033591+psilvagg@users.noreply.github.com>
2025-01-01 23:38:03 -0300

    Update get-ip.php

--- get-ip.php
+++ get-ip.php
@@ -1,8 +1,14 @@
 <?php
 
-if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $ip_do_usuario = $_SERVER['HTTP_X_FORWARDED_FOR'];
+//Busca IP do usuário, mesmo com Proxy
+// Busca IP do usuário, mesmo com Proxy
+$ip_usuario = $_SERVER["REMOTE_ADDR"];
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip_usuario = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip_usuario = $_SERVER['HTTP_X_FORWARDED_FOR'];
 } else {
-    $ip_do_usuario = $_SERVER['REMOTE_ADDR'];
+    $ip_usuario = $_SERVER['REMOTE_ADDR'];
 }
-echo $ip_do_usuario;
+
+echo $ip_usuario;
