24c6126f1c6dc0cf9d34cf8a93dcf000eea249e2
Pedro Silva <106033591+psilvagg@users.noreply.github.com>
2025-01-01 23:32:15 -0300

    Update get-ip.php

--- get-ip.php
+++ get-ip.php
@@ -1,3 +1,21 @@
 <?php
 
-echo $_SERVER['REMOTE_ADDR'];
+function obterIPreal()
+{
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) 
+{
+ $ip=$_SERVER['HTTP_CLIENT_IP'];
+}
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+
+{
+  $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+}
+else
+{
+ $ip=$_SERVER['REMOTE_ADDR'];
+}
+ echo $ip;
+}
+
+obterIPreal();
