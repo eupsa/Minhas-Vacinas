3e2dfc84e6e3f463bc7ad7b80f5a550b48d8cd42
Pedro Silva <106033591+psilvagg@users.noreply.github.com>
2025-01-04 22:15:34 -0300

    update

--- src/scripts/conn.php
+++ src/scripts/conn.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO("mysql:dbname=minhasvacinas;host=ls-e8bedc182761aa9145c04573adeaca0fa850a6bb.cfqcuaoygko7.us-east-2.rds.amazonaws.com", "dbmasteruser", "&{teqk)%5;32Ef5I*3fU04Bl7v3)8Fy3");
+$pdo = new PDO("mysql:dbname=minhasvacinas;host=ls-c731e648fc2ecc0a11bad015fbb20c99c0b0f5a4.crg0oeyac6i9.us-east-1.rds.amazonaws.com", "root", "Chicote1");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
