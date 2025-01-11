bcddbd970474cff7ce660883cfd04a73d73a077f
Pedro Silva <106033591+psilvagg@users.noreply.github.com>
2025-01-11 17:27:40 -0300

    upload

---
+++ sql/limpar-tabelas.sql
@@ -0,0 +1,16 @@
+-- SEMPRE EXECUTAR TUDO --
+use minhasvacinas;
+SET SQL_SAFE_UPDATES = 0;
+SET foreign_key_checks = 0;
+
+TRUNCATE TABLE confirmar_cadastro;
+TRUNCATE TABLE esqueceu_senha;
+TRUNCATE TABLE excluir_conta;
+TRUNCATE TABLE mudar_email;
+TRUNCATE TABLE usuario;
+TRUNCATE TABLE vacina;
+
+SET SQL_SAFE_UPDATES = 1;
+SET foreign_key_checks = 1;
+
+-- SEMPRE EXECUTAR TUDO --
