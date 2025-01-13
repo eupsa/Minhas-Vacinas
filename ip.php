eca86d048ead8196f73b826f76ba412f1dd9945d
Pedro Silva <106033591+psilvagg@users.noreply.github.com>
2025-01-12 21:04:36 -0300

    Update ip.php

--- ip.php
+++ ip.php
@@ -1,25 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <script>
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("GET", 'https://meuip.com/api/meuip.php', true); // A requisição é assíncrona
-        xmlhttp.send();
-        xmlhttp.onload = function(e) {
-            if (xmlhttp.status === 200) { // Verifica se a requisição foi bem-sucedida
-                alert("Seu IP é: " + xmlhttp.responseText); // Corrige para 'responseText' para obter a resposta correta
-            } else {
-                alert("Erro ao obter o IP.");
-            }
-        }
-    </script>
-</body>
-
-</html>
+<?php
+$url = "https://meuip.com/api/meuip.php";
+$response = file_get_contents($url);
+echo $response;
+?>
