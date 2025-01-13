90f8cafd987fa05674e0dfcc90130a3461ebf2dc
Pedro Silva <106033591+psilvagg@users.noreply.github.com>
2025-01-12 21:00:27 -0300

    Create ip.php

---
+++ ip.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- Embutindo o iframe para mostrar o IP do usuário -->
+    <iframe src="http://meuip.datahouse.com.br/meuip-site.php" name="meuip" width="525" height="132" scrolling="No" frameborder="0" id="meuip"></iframe>
+</body>
+
+</html>
