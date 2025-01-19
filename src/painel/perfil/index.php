086e8031c4f375ee985631543b7923501b41e6c1
Pedro Silva <106033591+psilvagg@users.noreply.github.com>
2025-01-19 20:24:45 -0300

    update

--- src/painel/perfil/index.php
+++ src/painel/perfil/index.php
@@ -159,8 +159,13 @@ if ($sql->rowCount() != 1) {
                 <div class="dropdown">
                     <a href="" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
                         id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="/assets/img/bx-user.svg" alt="Foto do Usuário" class="rounded-circle me-2"
-                            width="40" height="40">
+                        <?php if (isset($_SESSION['session_fotourl'])): ?>
+                            <img src="<?php echo $_SESSION['session_fotourl']; ?>" alt="Foto do Usuário" class="rounded-circle me-2"
+                                width="40" height="40">
+                        <?php else: ?>
+                            <img src="/assets/img/bx-user.svg" alt="Foto do Usuário" class="rounded-circle me-2"
+                                width="40" height="40">
+                        <?php endif; ?>
                         <span><?php echo isset($_SESSION['session_nome']) ? explode(' ', $_SESSION['session_nome'])[0] : 'Usuário'; ?></span>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
