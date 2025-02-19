88dd7867bc3622baabe9b862ddb0a2c8fad4d0b2
Pedro Silva <106033591+psilvagg@users.noreply.github.com>
2025-02-19 16:35:21 -0300

    Update index.php

--- src/admin/index.php
+++ src/admin/index.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login</title>
+    <link rel="shortcut icon" href="/assets/img/img-web.png" type="image/x-icon">
+    <title>Admin | Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         .form-signin {
@@ -17,11 +18,11 @@
 <body class="d-flex align-items-center py-4 bg-body-tertiary">
     <main class="form-signin w-100 m-auto text-center">
         <form style="margin-top: 50%;">
-            <img class=" mb-4" src="/assets/img/logo-head.png" alt="" width="100" height="90">
+            <img class=" mb-4" src="https://www.minhasvacinas.online/assets/img/logo-head.png" alt="" width="100" height="90">
             <h1 class="h3 mb-3 fw-normal">Faça login</h1>
             <div class="form-floating" style="margin-bottom: 5%;">
                 <input type="email" class="form-control" id="floatingInput" placeholder="email@example.com">
-                <label for="floatingInput">Email</label>
+                <label for="floatingInput">E-mail</label>
             </div>
             <div class="form-floating" style="margin-bottom: 5%;">
                 <input type="password" class="form-control" id="floatingPassword" placeholder="Senha">
