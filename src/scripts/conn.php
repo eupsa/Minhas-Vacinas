60e2c3611811e68d5aa3eb6074bf837abe1715ba
Pedro Silva <106033591+psilvagg@users.noreply.github.com>
2025-02-21 15:56:04 -0300

    Update conn.php

--- src/scripts/conn.php
+++ src/scripts/conn.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO("mysql:dbname=minhasvacinas;host=136.248.69.97", "root", "Chicote1@");
+$pdo = new PDO("mysql:dbname=minhasvacinas;host=152.67.40.5", "root", "184200");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
