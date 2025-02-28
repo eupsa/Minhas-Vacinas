ced77b5c9047fbd623eed3961b65612103bb52e5
Pedro Silva <106033591+psilvagg@users.noreply.github.com>
2025-02-28 18:12:37 -0300

    Update atualizar-dados.php

--- src/painel/backend/atualizar-dados.php
+++ src/painel/backend/atualizar-dados.php
@@ -62,7 +62,7 @@ if (!in_array($arquivoNew[sizeof($arquivoNew) - 1], ['jpg', 'png', 'jpeg'])) {
     $name = bin2hex(random_bytes(50)) . '.' . strtolower(end($arquivoNew));
 
     // Caminho de upload absoluto
-    $upload_dir = '/upload/';
+    $upload_dir = $_SERVER['DOCUMENT_ROOT'] . '/upload/';
 
     // Verifica se o diretório existe
     if (!is_dir($upload_dir)) {
