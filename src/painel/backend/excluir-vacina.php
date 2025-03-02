b42938df37b3faf8d6a0cfeb5650999f68cf0fbe
Pedro Araujo <106033591+psilvagg@users.noreply.github.com>
2025-03-02 17:19:07 -0300

    Upload

--- src/painel/backend/excluir-vacina.php
+++ src/painel/backend/excluir-vacina.php
@@ -23,6 +23,7 @@ if (empty($id_vac)) {
                 $path = explode('/',  $vacina['path_card'][4]);
 
                 $dir = '/var/www/Assets-MV/vac-img/' . $path .
+                var_dump($dir);
                 unlink($dir);
             }
 
